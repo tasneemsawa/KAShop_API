6e4f690e171dcb3d1810a65c112c61fc6d52041a
Tasneem Sawalha <tasneem31997@gmail.com>
2026-07-01 23:18:27 +0300

    Dto files

---
+++ KASHOP.DAL/Dto/CategoryTranslationResponse.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KASHOP.DAL.Dto
+{
+    public class CategoryTranslationResponse
+    {
+        public String Name { get; set; }
+        public String Language { get; set; }
+
+    }
+}
