f67b14e338b847b390c4d99de3db80cea8fce3b7
Tasneem Sawalha <tasneem31997@gmail.com>
2026-07-12 19:05:20 +0300

    Ignore .NET build files

--- KASHOP.DAL/obj/Debug/net9.0/KASHOP.DAL.AssemblyInfo.cs
+++ KASHOP.DAL/obj/Debug/net9.0/KASHOP.DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KASHOP.DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b166079982eaa5fcead38bde696b768ecbe731ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4531dad2f5da8a415b6d83e0cf8b14ef0c54e52f")]
 [assembly: System.Reflection.AssemblyProductAttribute("KASHOP.DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KASHOP.DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
