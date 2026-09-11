b52ae05236f6c646428780daaa43b7a7a0538bb1
Tasneem Sawalha <tasneem31997@gmail.com>
2026-09-11 20:23:58 +0300

    Email Sender secrets

--- KASHOP.BLL/Common/EmailSender.cs
+++ KASHOP.BLL/Common/EmailSender.cs
@@ -5,22 +5,30 @@ using System.Net;
 using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace KASHOP.BLL.Common
 {
     public class EmailSender : IEmailSender
     {
+        private readonly IConfiguration _config;
+
+        public EmailSender(IConfiguration config)
+        {
+            _config = config;
+        }
         public Task SendEmailAsync(string email, string subject, string message)
         {
-            var client = new SmtpClient("smtp.gmail.com", 587)
+            var client = new SmtpClient(_config["EmailSettings:Host"], int.Parse(_config["EmailSettings:Port"]))//var client = new SmtpClient("smtp.gmail.com", 587)
             {
                 EnableSsl = true,
                 UseDefaultCredentials = false,
-                Credentials = new NetworkCredential("tasneem31997@gmail.com", "vpqv mlsu pbwm stxp")
+                Credentials = new NetworkCredential(_config["EmailSettings:Email"], _config["EmailSettings:Password"])  //Credentials = new NetworkCredential("tasneem31997@gmail.com", "vpqv mlsu pbwm stxp")
             };
 
             return client.SendMailAsync(
-                new MailMessage(from: "tasneem31997@gmail.com",
+                new MailMessage(from:_config["EmailSettings:Email"], //"tasneem31997@gmail.com",
                                 to: email,
                                 subject,
                                 message
--- KASHOP.PL/obj/Debug/net9.0/KASHOP.PL.AssemblyInfo.cs
+++ KASHOP.PL/obj/Debug/net9.0/KASHOP.PL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KASHOP.PL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c36c6de7f9775441b3dcca23d6998251f11a79fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+094c7a51bc537629cf66c447979c94cf256e9211")]
 [assembly: System.Reflection.AssemblyProductAttribute("KASHOP.PL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KASHOP.PL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
